2fe1cc8fd10eee5bba7f25aabc0caa70df4cda82
sonoranrisebakery-prog <sonoranrisebakery@gmail.com>
2026-07-10 02:52:55 -0700

    Replace Astro starter with Sonoran Rise branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Sonoran Rise Bakery";
+export const SITE_DESCRIPTION = "Phoenix artisan bakery creating 100% freshly milled whole grain sourdough bread.";
